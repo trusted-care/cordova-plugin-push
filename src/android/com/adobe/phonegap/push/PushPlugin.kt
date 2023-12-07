8442dc6f0e0e9d0a403d8d513035088c5045b8a3
Pavel Petrov <pppetrv@gmail.com>
2023-12-07 16:16:52 +0300

    fix conflicting POST_NOTIFICATION permission request with other plugins

--- src/android/com/adobe/phonegap/push/PushPlugin.kt
+++ src/android/com/adobe/phonegap/push/PushPlugin.kt
@@ -629,11 +629,11 @@ class PushPlugin : CordovaPlugin() {
     private fun checkForPostNotificationsPermission(): Boolean {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             if (!PermissionHelper.hasPermission(this, Manifest.permission.POST_NOTIFICATIONS)) {
-                PermissionHelper.requestPermission(
-                    this,
-                    REQ_CODE_INITIALIZE_PLUGIN,
-                    Manifest.permission.POST_NOTIFICATIONS
-                )
+                //PermissionHelper.requestPermission(
+                //    this,
+                //    REQ_CODE_INITIALIZE_PLUGIN,
+                //    Manifest.permission.POST_NOTIFICATIONS
+                //)
                 return false
             }
         }
